3800c39de13332f3237d88dcfbd2751dc314a31a
Zakaria MAIT <ziko.mait14@gmail.com>
2023-09-28 01:23:39 +0100

    Initial commit

---
+++ resources/js/Components/Navigation.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Navigation
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="h-screen bg-gray-50 flex w-full gap-4">
+        <Navigation />
+
+        <main class="flex flex-col flex-1 px-4 overflow-hidden">
+            <div class="flex items-center"></div>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import Navigation from "@/Components/Navigation.vue";
+
+</script>
